7c9ad8304f9ac9fd0882abf9d148a4d7192791d3
rizkitirta <rizkibuwono0@gmail.com>
2022-06-02 21:28:30 +0700

    handler

---
+++ handler_test.go
@@ -0,0 +1,23 @@
+package golangweb
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, req *http.Request){
+		fmt.Fprintf(writer,"Hello World")
+	}
+
+	server := http.Server{
+		Addr: "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)  
+	}
+}
