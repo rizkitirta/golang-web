f6a76cc2eeb650e0f4a87ad5cf18152919d02c2c
rizkitirta <rizkibuwono0@gmail.com>
2022-06-02 22:02:40 +0700

    ServeMux (Router In Golang)

--- handler_test.go
+++ handler_test.go
@@ -21,3 +21,32 @@ func TestHandler(t *testing.T) {
 		panic(err)  
 	}
 }
+
+func TestServeMux(t *testing.T)  {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/",func (writer http.ResponseWriter, req *http.Request)  {
+		fmt.Fprint(writer,"Welcome To Golang Web")
+	})
+
+	mux.HandleFunc("/hello-world",func(writer http.ResponseWriter,req *http.Request){
+		fmt.Fprint(writer,"Hello World")
+	})
+
+	mux.HandleFunc("/images/",func(writer http.ResponseWriter,req *http.Request){
+		fmt.Fprint(writer,"images")
+	})
+	mux.HandleFunc("/images/thumbnails/",func(writer http.ResponseWriter,req *http.Request){
+		fmt.Fprint(writer,"Thumbnails")
+	})
+
+	server := http.Server {
+		Addr: "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)	
+	}
+}
